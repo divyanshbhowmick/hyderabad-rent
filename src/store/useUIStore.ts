47d912dce0e84a5892c5b1cbe433aa745280187f
divyanshbhowmick <divyansh.bhowmick3@gmail.com>
2026-04-17 00:05:24 +0530

    feat: add emailClaim modal type and pendingPinId to useUIStore

--- src/store/useUIStore.ts
+++ src/store/useUIStore.ts
@@ -2,29 +2,32 @@
 import { create } from 'zustand'
 import type { Pin } from '../types/Pin'
 
-type Modal = 'onboarding' | 'pinSubmit' | 'pinDetail' | 'filter' | null
+type Modal = 'onboarding' | 'pinSubmit' | 'pinDetail' | 'filter' | 'emailClaim' | null
 
 interface UIStore {
   activeModal: Modal
   pendingLatLng: { lat: number; lng: number } | null
   selectedPin: Pin | null
+  pendingPinId: string | null
   openModal: (modal: Modal) => void
   closeModal: () => void
   setPendingLatLng: (latlng: { lat: number; lng: number }) => void
   setSelectedPin: (pin: Pin | null) => void
+  setPendingPinId: (id: string | null) => void
 }
 
 export const useUIStore = create<UIStore>((set) => ({
   activeModal: null,
   pendingLatLng: null,
   selectedPin: null,
+  pendingPinId: null,
 
   openModal(modal) {
     set({ activeModal: modal })
   },
 
   closeModal() {
-    set({ activeModal: null, pendingLatLng: null, selectedPin: null })
+    set({ activeModal: null, pendingLatLng: null, selectedPin: null, pendingPinId: null })
   },
 
   setPendingLatLng(latlng) {
@@ -33,5 +36,9 @@ export const useUIStore = create<UIStore>((set) => ({
 
   setSelectedPin(pin) {
     set({ selectedPin: pin })
+  },
+
+  setPendingPinId(id) {
+    set({ pendingPinId: id })
   },
 }))
